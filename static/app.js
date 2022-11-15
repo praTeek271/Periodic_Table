3db2f9a8eb58b576fec3c65a7b6ac941a657e943
praTeek271 <prateekpatriotasme@gmail.com>
2022-11-15 22:15:23 +0530

    box_bug_fixed

--- static/app.js
+++ static/app.js
@@ -3,15 +3,12 @@ const box=document.getElementById("pbox");
 var show=1;
 var hide=0;
 
-function screen_on(atomic_no) {
+function screen_on(atomic_no,symbol,name,atomic_mass,xpos,ypos,cpk_hex) {
   
   console.log(`Showing Screen`);
   diplay_screen.style.opacity=show;
   box.classList.toggle('blur');
-  
-  
 
-  // counter+=1;
   diplay_screen.innerHTML =atomic_no;
   console.log(atomic_no+"<----- : added");
   diplay_screen.onclick=function() {
